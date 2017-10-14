2d06132aaa6cf835c056a387e26f0a30ed878dd4
Grijesh Saini <grijesh.saini@digital.hmrc.gov.uk>
2017-10-14 22:34:37 +0100

    Post request to send notification

--- web/src/main/java/com/learning/notifier/controller/NotificationController.java
+++ web/src/main/java/com/learning/notifier/controller/NotificationController.java
@@ -1,8 +1,12 @@
 package com.learning.notifier.controller;
 
 
+import com.learning.notifier.model.Notification;
 import com.learning.notifier.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +17,11 @@ public class NotificationController {
     @Autowired
     public NotificationController(NotificationService notificationService) {
         this.notificationService = notificationService;
+    }
+
+    @PostMapping("/notifier/send")
+    public ResponseEntity<String> sendNotification(@RequestBody Notification notification) {
+        notificationService.sendMessage(notification);
+        return ResponseEntity.ok("Sent");
     }
 }
--- web/src/test/java/com/learning/notifier/controller/NotificationControllerTest.java
+++ web/src/test/java/com/learning/notifier/controller/NotificationControllerTest.java
@@ -1,24 +1,42 @@
 package com.learning.notifier.controller;
 
 import com.learning.notifier.model.Notification;
+import com.learning.notifier.service.NotificationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 
 class NotificationControllerTest {
 
     private NotificationController notificationController;
 
+    @Mock
+    private NotificationService notificationService;
+
+    @BeforeEach
+    public void setup() {
+        initMocks(this);
+        notificationController = new NotificationController(notificationService);
+    }
 
 
     @Test
     @DisplayName("Should able to send Notification")
-    public void should_send_notification(){
+    public void should_send_notification() {
         Notification notification = new Notification("12312412", "Android", "Hellp");
+        doNothing().when(notificationService).sendMessage(any());
 
-
+        ResponseEntity<String> response = notificationController.sendNotification(notification);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("Sent", response.getBody());
     }
 
 }
